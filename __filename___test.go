df6a756816f735423f3b540b3d69a26744f3b216
Damon Douglas <douglas.damon@gmail.com>
2016-06-10 01:12:27 -0700

    Add setup and teardown methods

--- __filename___test.go
+++ __filename___test.go
@@ -3,8 +3,24 @@ package __package___test
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"os"
 )
+
+func Setup() {
+
+}
+
+func Teardown() {
+
+}
 
 func Test__funcname__(t *testing.T) {
   assert.True(t, true, "true should be true")
+}
+
+func TestMain(m *testing.M) {
+  Setup()
+  result := m.Run()
+  Teardown()
+  os.Exit(result)
 }
